95dcaf86aebbfe60dab1b74c2b21a6b2809b4d7d
andrewmcg1 <greekgeek191@gmail.com>
2023-12-06 15:04:40 -0500

    fixed something

--- optimization.m
+++ optimization.m
@@ -1,9 +1,14 @@
 
 
-params = [2, 4, 12];
-fuel_mass_4series(params)
+params = [2, 4, 12, 10, 120, .3];
+% fuel_mass_4series(params)
 
-function fm = fuel_mass_4series(v)
+min_guess = [0, 0, 11, 5, 50, 0];
+max_guess = [9, 9, 30, 50, 250, 1];
+
+a = fmincon(@fuel_mass_4series, params, [], [], [], [], min_guess, max_guess, @constraint)
+
+function E_co2 = fuel_mass_4series(v)
     global cl
     global cd
     global NACA34
@@ -11,35 +16,9 @@ function fm = fuel_mass_4series(v)
     NACA1 = round(v(1));
     NACA2 = round(v(2));
     NACA34 = round(v(3));
-
-    alpha = 0:20;
-    Re = 1e6; % Reynolds number
-    cruise_mach = .65;
-
-    airfoil = append('NACA', int2str(NACA1), int2str(NACA2), sprintf('%02d', NACA34));
-    [pol foil] = xfoil(airfoil,alpha,Re,cruise_mach,'ppar n 100','oper iter 200');
-    
-    [cl, i] = max(pol.CL);
-    cd = pol.CD(i);
-
-
-    min_guess = [1, 50, 0];
-    max_guess = [100, 1550, 1];
-
-    guess = [5, 250, .01];
-
-    [params, E_co2] = fmincon(@helper, guess, [], [], [], [], min_guess, max_guess, @constraint, optimset('MaxFunEvals', 100000, 'MaxIter', 100000)')
-
-    fm = [E_co2, params];
-end
-
-function E_co2 = helper(v)
-    global cl
-    global cd
-    global NACA34
-    c = v(1);
-    b = v(2);
-    fuel_frac = v(3);
+    c = v(4);
+    b = v(5);
+    fuel_frac = v(6);
 
     FCF = 3.16; % Fuel consumption factor
     LS_f = (89 - 30 * fuel_frac); % lifetime emissions
@@ -57,11 +36,21 @@ function E_co2 = helper(v)
 
     n = (span_e * AR) / (span_e * AR + 2);
 
-    CL = cl * n;
-    CD = cd + CL^2 / (pi * span_e * AR);
+    alpha = -5:20;
+    speed_sound = 295; % m/s
+    velocity = cruise_mach * speed_sound;
+    viscosity = 1.458e-5;
+    Re = rho_inf * velocity * c / viscosity % Reynolds number
 
-    LpD = CL/CD;
-    %[LpD, i] = max(CL./CD);
+    airfoil = append('NACA', int2str(NACA1), int2str(NACA2), sprintf('%02d', NACA34));
+    [pol foil] = xfoil(airfoil,alpha,Re,cruise_mach,'ppar n 100','oper iter 200');
+
+    CL = pol.CL * n;
+    CD = pol.CD + CL.^2 / (pi * span_e * AR);
+
+    [LpD, i] = max(CL./CD);
+    CL = CL(i);
+    CD = CD(i);
 
     d_hf = (42.8 - 20 * fuel_frac^2);
     Sw = b * c;
@@ -83,6 +72,9 @@ function E_co2 = helper(v)
     h = E_co2 + (L - w0)^2;
 
     range
+    Re
+    c
+    b
 end
 
 function [cc,ceq] = constraint(v)
